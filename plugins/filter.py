fbd269404dcd0fa7d95b71cb18d3443d7f181880
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2022-03-30 00:59:57 +0700

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -30,7 +30,7 @@ from . import events, get_string, mediainfo, udB, ultroid_bot, ultroid_cmd
 from ._inline import something
 
 
-@ultroid_cmd(pattern="addfilter( (.*)|$)")
+@ultroid_cmd(pattern="cptfilter( (.*)|$)")
 async def af(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
